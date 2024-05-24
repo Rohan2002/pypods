0555ce94a7228b55cd1075a378f43aadfc1054ba
Rohan2002 <rohandeshpande832@gmail.com>
2024-05-24 14:55:10 -0400

    fix: integration test, still need unittest

--- tests/test_pods.py
+++ tests/test_pods.py
@@ -1,48 +1,60 @@
 import unittest
-from pypods.pods import PodLoader, PodListener
+from pypods.pods import PodLoader
 from pypods.errors import PyPodResponseError
 import json
-from bson import dumps, loads
+import os
+from os.path import join, exists
+import shutil
+
+# TODO: Unittest using mock, and patch functions for PodLoader and PodListener
 class TestPodLoader(unittest.TestCase):
     def setUp(self) -> None:
-        self.ns = {}
-        self.pl = PodLoader(pod_name="test-pod", namespace=self.ns)
+        pass
     def test_load_pod(self):
-        self.pl.load_pod()
-
-        self.assertTrue("foo1" in self.pl.namespace)
-        self.assertTrue("foo2" in self.pl.namespace)
-        
-        # Make the functions from pod got replaced with rpc_proxy_functions
-        self.assertTrue(self.pl.namespace["foo1"].__name__ == "rpc_proxy_function")
-        self.assertTrue(self.pl.namespace["foo2"].__name__ == "rpc_proxy_function")
+        pass
     def test_unload_pod(self):
-        self.pl.load_pod()
-        self.assertTrue(len(self.pl.namespace) != 0)
-        self.pl.unload_pod()
-        self.assertTrue(len(self.pl.namespace) == 0)
-
+        pass
     # Integration test.
-    # This will call load functions from pod, call them
-    # and check if their output is correct and finally unload all the functions.
-    def test_functions(self):
-        self.pl.load_pod()
-        foo_out = self.ns["foo1"](1, 2)
+    # Create pod.
+    # Load functions into a namespace from pod.
+    # Call functions and check expected == actual output.
+    # Unload functions from namespace.
+    def test_pypod(self):
+        pl = PodLoader(pod_name="test_pod", namespace={})
+        root_dir = os.path.dirname(os.path.abspath(__file__))
+        fixture_dir = join(root_dir, "fixtures")
+
+        # Setup pod files
+        shutil.copy(join(fixture_dir, "obj.json"), f"pods/{pl.pod_name}/obj.json")
+        shutil.copy(join(fixture_dir, "pod.py"), f"pods/{pl.pod_name}/pod.py")
+
+        self.assertTrue(exists(f"pods/{pl.pod_name}/obj.json"))
+        self.assertTrue(exists(f"pods/{pl.pod_name}/pod.py"))
+        
+        # Create pod.
+        # Load functions into a namespace from pod.
+        pl.load_pod()
+
+        # Call functions and check expected == actual output.
+        foo_out = pl.namespace["foo1"](1, 2)
         self.assertEqual(foo_out, 3)
         
-        foo2_out = self.ns["foo2"]()
+        foo2_out = pl.namespace["foo2"]()
         self.assertEqual(foo2_out, "A")
 
-        foo3_out = self.ns["foo3"]()
+        foo3_out = pl.namespace["foo3"]()
         expected = None
-        with open("fixtures/obj.json", mode="r") as o:
+        with open(join(fixture_dir, "obj.json"), mode="r") as o:
             expected = json.load(o)
         self.assertEqual(foo3_out, expected)
 
+        # Invalid function calls.
         with self.assertRaises(expected_exception=PyPodResponseError):
-            foo2_bad_out = self.ns["foo2"]("No-arg") # No arg should be provided for foo2
+            foo2_bad_out = pl.namespace["foo2"]("No-arg") # No arg should be provided for foo2
         
-        self.pl.unload_pod()
+        # Unload functions from namespace.
+        pl.unload_pod()
+        self.assertEqual(len(pl.namespace), 0)
 
 if __name__ == "__main__":
     unittest.main()
